66cff4f68bf62a8a1907fc9bc7f18fb886d39588
ta <ta@example.com>
2019-02-15 02:29:10 +0900

    chart表示

---
+++ load_file_and_show_chart/src/components/ShowFigures.vue
@@ -0,0 +1,26 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  mounted() {
+    this.renderChart({
+      labels: ['1', '2', '3', '4', '5', '6', '7'],
+      datasets: [
+        {
+          label: 'Chart',
+          backgroundColor: '#2c3e50',
+          data: [0, 30, 60, 0, 60, 30, 0]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+}
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
