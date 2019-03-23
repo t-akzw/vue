56bcdb12f83e09c96b508160a41ce643aa03d8a6
ta <ta@example.com>
2019-03-24 02:06:09 +0900

    カテゴリ部分新規実装

--- household_account_book/src/App.vue
+++ household_account_book/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Input msg="Household account book(Ver.01)"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Input
   }
 }
 </script>
